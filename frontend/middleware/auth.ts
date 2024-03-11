04a758ec950e5bf88518cd809974779a803cfd1f
Hillar Kapsta <kapstahillar@gmail.com>
2024-03-11 23:37:09 +0200

    Fix middleware

--- frontend/middleware/auth.ts
+++ frontend/middleware/auth.ts
@@ -3,8 +3,6 @@ import { useAuth } from "../api/composables/useAuth";
 export default defineNuxtRouteMiddleware(() => {
     const { isAuthenticated } = useAuth();
     const config = useRuntimeConfig();
-
-    console.log(config.public.loginUrl)
     if (isAuthenticated.value === false) {
         return navigateTo(config.public.loginUrl as string, { replace: true });
     }
--- frontend/middleware/guest.ts
+++ frontend/middleware/guest.ts
@@ -3,7 +3,6 @@ import { useAuth } from "../api/composables/useAuth";
 export default defineNuxtRouteMiddleware(() => {
     const { isAuthenticated } = useAuth();
     const config = useRuntimeConfig();
-
     if (isAuthenticated.value === true) {
         return navigateTo(config.public.homeUrl as string, { replace: true });
     }
