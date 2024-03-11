af9c31845b70ba3c9a736ac3ad33418c924596fe
Hillar Kapsta <kapstahillar@gmail.com>
2024-03-11 23:31:35 +0200

    Add game database migration

---
+++ api/database/migrations/2024_03_11_161452_create_gamesessions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('game_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('fields')->default('["-","-","-","-","-","-","-","-","-"]');
+            $table->integer('difficulty')->default(0);
+            $table->integer('winner')->nullable();
+            $table->dateTime('completed_at')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('game_sessions');
+    }
+};
