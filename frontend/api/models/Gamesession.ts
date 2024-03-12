90d3d62476641418202841818b28047341623b86
Hillar Kapsta <kapstahillar@gmail.com>
2024-03-12 17:18:02 +0200

    Update game session model

--- frontend/api/models/Gamesession.ts
+++ frontend/api/models/Gamesession.ts
@@ -7,7 +7,7 @@ export enum GameFieldValue {
 export default interface GameSession {
     id: number
     user_id: number // Current user playing
-    fields: Array<GameFieldValue> // Length always 9
+    state: string // State in string values
     created_at: Date
     winner: number | null
     completed_at: Date
