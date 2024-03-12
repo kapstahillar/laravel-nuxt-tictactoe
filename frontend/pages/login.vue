5d9ae6e098297e689d28fdae82a5ba40bd17df46
Hillar Kapsta <kapstahillar@gmail.com>
2024-03-12 20:36:55 +0200

    Update login view

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,54 +1,18 @@
+<template>
+    <ElMain>
+        <ElCard class="loginpage__container-card">
+            <LoginForm />
+        </ElCard>
+    </ElMain>
+</template>
 <script lang="ts" setup>
-import { useAuthStore } from '~/store/auth.store';
 definePageMeta({
     middleware: ["guest"],
 });
-
-interface Credentials {
-    username: string;
-    password: string;
-}
-
-const { login } = useAuth();
-const config = useRuntimeConfig();
-const router = useRouter();
-
-const credentials: Credentials = reactive({
-    username: "",
-    password: "",
-});
-
-const error = ref<string>("");
-const auth = useAuthStore()
-async function submit() {
-    try {
-        error.value = "";
-        await login(credentials.username, credentials.password, true);
-        if (auth.isAuthenticated)
-            router.push(config.public.homeUrl);
-    } catch (err) {
-        error.value = err as string;
-    }
-}
 </script>
-
-<template>
-    <div>
-        <p>Page: login</p>
-
-        <form @submit.prevent="submit">
-            <small>{{ error }}</small>
-
-            <input id="username" v-model="credentials.username" type="text" name="username" placeholder="Your username"
-                autocomplete="off" />
-            <input id="password" v-model="credentials.password" type="password" name="password"
-                placeholder="Your password" autocomplete="off" />
-
-            <button type="submit">Login</button>
-        </form>
-
-        <NuxtLink to="/register" class="text-blue-500">
-            Register
-        </NuxtLink>
-    </div>
-</template>
+<style scoped>
+.loginpage__container-card {
+    max-width: 480px;
+    margin: auto;
+}
+</style>
